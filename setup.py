e9e89dd45ff41170e493c977131fdea40de25604
Mr-Milk <zym.zym1220@gmail.com>
2023-02-07 14:40:55 +0800

    use flit for build system

--- setup.py
+++ setup.py
@@ -4,20 +4,4 @@ from setuptools import setup
 
 README = Path("README.md").read_text()
 
-setup(name="python-hmr",
-      packages=['hmr'],
-      description="Hot module reload for python",
-      long_description=README,
-      long_description_content_type="text/markdown",
-      version="0.1.1",
-      author="Mr-Milk",
-      url="https://github.com/Mr-Milk/python-hmr",
-      author_email="zym.zym1220@gmail.com",
-      license="MIT License",
-      classifiers=[
-          "License :: OSI Approved :: MIT License",
-          "Programming Language :: Python :: 3",
-      ],
-      python_requires='>=3.6',
-      install_requires=['watchdog'],
-      )
+setup(name="python-hmr")
